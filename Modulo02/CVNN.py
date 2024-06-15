e36fd4ccf4f22bdf536333985c8f799330d9fcd9
Mathias <mathiasaugustodacruz@gmail.com>
2024-06-15 10:20:53 -0300

    Exercicios Resolvidos

---
+++ Modulo02/CVNN.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+# Define the CNN model
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model 
+model.fit(train_images, train_labels, epochs=5)
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('Test accuracy:', test_acc)
